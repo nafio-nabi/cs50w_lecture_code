bbfca7c3015ae7b30e7035c86ae73ef0d27f9901
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-03-02 14:39:27 +0600

    sequences

---
+++ 3_python/sequences.py
@@ -0,0 +1,16 @@
+name = "Harry"
+
+print(name[0])
+print(name[1])
+
+names = ["Harry", "Ron", "Hermione"]
+
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+
+coordinate = (10.0, 20.0)
+print(coordinate)
+print(coordinate[0])
+print(coordinate[1])
