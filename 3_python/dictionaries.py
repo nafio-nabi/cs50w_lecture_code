ee2d1847adeb59823e59e93cb9784ac220235218
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-03-02 15:07:18 +0600

    dictionaries

---
+++ 3_python/dictionaries.py
@@ -0,0 +1,9 @@
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+print(houses)
+print(houses["Harry"])
+print(houses["Draco"])
+
+houses["Hermione"] = "Gryffindor"
+
+print(houses)
+print(houses["Hermione"])
