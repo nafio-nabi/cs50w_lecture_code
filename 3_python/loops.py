b03db59f864c7699de191ab98b2354ba66e7748b
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-03-02 15:02:11 +0600

    loops

---
+++ 3_python/loops.py
@@ -0,0 +1,19 @@
+for i in [0, 1, 3, 4, 5]:
+    print(i)
+
+print()
+
+for i in range(6):
+    print(i)
+
+print()
+
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+for name in names:
+    print(name)
+
+print()
+
+name = "Harry"
+for char in name:
+    print(char)
