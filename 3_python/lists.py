e50404f18955dde2ceb859f830bb7abcd5eef05a
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-03-02 14:51:27 +0600

    lists

---
+++ 3_python/lists.py
@@ -0,0 +1,15 @@
+# Define a list of names.
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+print(names)
+print(names[3])
+
+names.append("Draco")
+
+print(names)
+print(names[4])
+
+names.sort()
+
+print(names)
+print(names[4])
