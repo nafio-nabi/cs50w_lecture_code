af14290cd90dea1f30ced245b03ba888c38d19ae
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-03-03 09:10:26 +0600

    functions and modules

---
+++ 3_python/squares.py
@@ -0,0 +1,8 @@
+# import functions
+from functions import square
+
+# for i in range(10):
+#     print(f"The square of {i} is {functions.square(i)}.")
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}.")
