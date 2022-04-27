5934168e9bed163d6227c09964464b657a587da6
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-04-27 10:35:04 +0600

    assert

---
+++ 7_testing_ci-cd/assert.py
@@ -0,0 +1,24 @@
+# 1
+# def square(x):
+#     return x * x
+
+# print(square(10))
+
+# 2
+# def square(x):
+#     return x * x
+
+# print(square(10) == 100)
+# print(square(10) == 101)
+
+# 3
+# def square(x):
+#     return x * x
+
+# assert square(10) == 100
+
+# 4
+def square(x):
+    return x + x
+
+assert square(10) == 100
